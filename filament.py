61882c4557c027aa5912c0e51f6b4301e558280d
Marwa ElDiwiny <marwaeldiwiny1991@gmail.com>
2023-12-02 20:36:05 +0100

    Create filament.py

---
+++ filament.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+
+import Sofa
+
+import os
+
+path = os.path.dirname(os.path.abspath(__file__)) + '/mesh/'
+
+
+def createScene(rootNode):
+    rootNode.addObject('RequiredPlugin', name='SoftRobots')
+    rootNode.addObject('RequiredPlugin', name='SofaPython3')
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.StateContainer')
+    
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Constraint.Projective') # Needed to use components [FixedConstraint]  
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Mapping.Linear') # Needed to use components [BarycentricMapping] 
+    
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.MechanicalLoad') # Needed to use components [ConstantForceField]  
+     
+     
+    rootNode.addObject('RequiredPlugin', pluginName=[
+        "Sofa.Component.AnimationLoop",  # Needed to use components FreeMotionAnimationLoop
+        "Sofa.Component.Constraint.Lagrangian.Correction",
+        # Needed to use components LinearSolverConstraintCorrection
+        "Sofa.Component.Constraint.Lagrangian.Solver",  # Needed to use components GenericConstraintSolver
+        "Sofa.Component.Engine.Select",  # Needed to use components BoxROI
+        "Sofa.Component.IO.Mesh",  # Needed to use components MeshSTLLoader, MeshVTKLoader
+        "Sofa.Component.LinearSolver.Direct",  # Needed to use components SparseLDLSolver
+        "Sofa.Component.Mass",  # Needed to use components UniformMass
+        "Sofa.Component.ODESolver.Backward",  # Needed to use components EulerImplicitSolver
+        "Sofa.Component.Setting",  # Needed to use components BackgroundSetting
+        "Sofa.Component.SolidMechanics.FEM.Elastic",  # Needed to use components TetrahedronFEMForceField
+        "Sofa.Component.SolidMechanics.Spring",  # Needed to use components RestShapeSpringsForceField
+        "Sofa.Component.Topology.Container.Constant",  # Needed to use components MeshTopology
+        "Sofa.Component.Visual",  # Needed to use components VisualStyle
+        "Sofa.GL.Component.Rendering3D",  # Needed to use components OglModel
+    ])
+    rootNode.addObject('VisualStyle', displayFlags="showVisualModels hideBehaviorModels hideCollisionModels \
+                        hideBoundingCollisionModels hideForceFields showInteractionForceFields hideWireframe")
+
+    rootNode.addObject('FreeMotionAnimationLoop')
+    rootNode.addObject('DefaultVisualManagerLoop')
+
+    rootNode.addObject('GenericConstraintSolver', maxIterations=1000, tolerance=1e-3)
+
+    #rootNode.addObject('BackgroundSetting', color=[0, 0.168627, 0.211765, 1])
+    rootNode.addObject('BackgroundSetting', color=[0, 0, 0, 0])
+    #rootNode.findData('gravity').value = [0, 0, -981.0]
+    rootNode.findData('gravity').value = [0, 0, -9.81]
+  
+    rootNode.findData('dt').value = 0.01
+    
+
+     
+    ##########################################
+    # python script controller to control tensile force  
+    ##########################################
+  
+    #rootNode.createObject('PythonScriptController', classname="controller", filename="force_controller.py")
+    
+    
+  ##########################################
+    # FEM Model                              #
+    ##########################################
+    matrix = rootNode.addChild('matrix')
+    matrix.addObject('EulerImplicitSolver', firstOrder=False, rayleighStiffness=0.2, rayleighMass=0.2)
+    matrix.addObject('SparseLDLSolver')
+
+    matrix.addObject('MeshVTKLoader', name='loader', filename=path + 'matrix.vtk', rotation=[0, 0, 0])
+    matrix.addObject('MeshTopology', src='@loader')
+
+    matrix.addObject('MechanicalObject', name='tetras', template='Vec3')
+    matrix.addObject('UniformMass', totalMass=1)
+    matrix.addObject('TetrahedronFEMForceField', template='Vec3', name='FEM', method='large', poissonRatio=0.5,
+                        youngModulus=0.6e6)
+    matrix.addObject('FixedConstraint', indices="82 84 85 308 309 496 503 584 681 843 638 640")
+    
+    matrix.addObject('LinearSolverConstraintCorrection')
+    
+    matrix.addObject('ConstantForceField',name='totalForce', indices= "217 218 226 298 469 480 483 516 554 558 565 569 571 607 631 693 721 727 792 834 897 929 930 933 935 1014 1129 1191 1219 1332 1431 1460", force='0 400000 0 0 0 0', showArrowSize = "0.00005")
+      
+     
+
+    
+    
+     # matrix visualization                     
+    ##########################################
+    # In Sofa, visualization is handled by adding a rendering model.
+    #  Create an empty child node to store this rendering model.
+    matrixVisu = matrix.addChild('visu0')
+
+    # Add to this empty node a rendering model made of triangles and loaded from a stl file.
+    matrixVisu.addObject('MeshSTLLoader', filename=path + "matrix.stl", name="loader")
+    matrixVisu.addObject('OglModel', src="@loader",  color=[0.7, 0.9, 0.3, 0.2]) 
+
+    # Add a BarycentricMapping to deform the rendering model in a way that follow the ones of the parent mechanical model.
+    matrixVisu.addObject('BarycentricMapping')
+
+    ##########################################
+    # filament                           #
+    ##########################################
+    #  This add a new node in the scene. This node is appended to the matrix's node.
+    filament = matrix.addChild('filament')
+    filament.addObject('EulerImplicitSolver', firstOrder=False, rayleighStiffness=0.2, rayleighMass=0.2)
+    filament.addObject('SparseLDLSolver')
+    #  This adds a MechanicalObject, a component holding the degree of freedom of our
+    # mechanical modelling. In the case of a pneumatic actuation it is a set of positions describing the spider wall.
+    filament.addObject('MeshVTKLoader', name='loader', filename=path + 'filament.vtk', rotation=[0, 0, 0])
+    filament.addObject('MeshTopology', src='@loader', name='topo')
+    filament.addObject('MechanicalObject', name='filament')
+    filament.addObject('TetrahedronFEMForceField', template='Vec3', name='FEM', method='large', poissonRatio=0.4,
+                        youngModulus=9e6)
+    filament.addObject('UniformMass', totalMass=0.003)
+
+
+
+    # This adds a BarycentricMapping. A BarycentricMapping is a key element as it will add a bi-directional link
+    #  between the spider wall (surfacic mesh) and the matrix (volumetric mesh) so that movements of the spider's DoFs will be mapped
+    #  to the matrix and vice-versa;
+    filament.addObject('BarycentricMapping', name='mapping', mapForces=False, mapMasses=False)
+
+    ##########################################
+    # filament Visualization                          #
+    ##########################################
+    filamentVisu = filament.addChild('visu1')
+    filamentVisu.addObject('MeshSTLLoader', filename=path + "filament.stl", name="loader")
+    filamentVisu.addObject('OglModel', src="@loader", color=[2, 3, 2, 9])
+    filamentVisu.addObject('BarycentricMapping')
+
+    return rootNode
