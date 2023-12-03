da1168561ba7a8f275e460ea55470553591e0ae6
Marwa ElDiwiny <marwaeldiwiny1991@gmail.com>
2023-12-03 17:08:03 +0100

    Update filament.py

--- filament.py
+++ filament.py
@@ -24,11 +24,28 @@ class SpiderController(Sofa.Core.Controller):
 		Sofa.Core.Controller.__init__(self,*args, **kwargs) #needed
 		self.time = 0.0
 		self.node = kwargs['node']
-		self.pos3 = kwargs['pos3']
+		self.pos_matrix = kwargs['pos_matrix']
+		self.pos_filament = kwargs['pos_filament']
 		
 	def onAnimateBeginEvent(self,event):
 		self.time = self.node.time.value
-		self.node.matrix.FF.force.value = [0,400000,0] 
+		
+		#### at "zero" strain, force "0", filament length is "9.3", active matrix length is "37.5" 
+		
+		# "from here "position Constraint below" can be used given the initial length and desired strain"
+		self.node.matrix.FF.force.value = [0,0,0]
+		
+		#### at "50%" strain,  force is "110000"
+		#self.node.matrix.FF.force.value = [0,300000,0]   
+		
+		
+		self.pos_filament= self.node.matrix.filament.l_filament.position.value
+		self.pos_matrix= self.node.matrix.l_matrix.position.value
+		
+		print(self.pos_filament)
+		
+		
+		
 
 
 def createScene(rootNode):
@@ -75,7 +92,7 @@ def createScene(rootNode):
 
   
     #rootNode.findData('dt').value = 0.01
-    
+     
 
      
     ##########################################
@@ -95,20 +112,20 @@ def createScene(rootNode):
     matrix.addObject('MeshVTKLoader', name='loader', filename=path + 'matrix.vtk', rotation=[0, 0, 0])
     matrix.addObject('MeshTopology', src='@loader')
 
-    matrix.addObject('MechanicalObject', name='tetras', template='Vec3')
+    matrix.addObject('MechanicalObject', name='l_matrix', template='Vec3')
     matrix.addObject('UniformMass', totalMass=1)
-    matrix.addObject('TetrahedronFEMForceField', template='Vec3', name='FEM', method='large', poissonRatio=0.5,
+    matrix.addObject('TetrahedronFEMForceField', template='Vec3', name='FEM', method='large', poissonRatio=0.4,
                         youngModulus=0.6e6)
-    matrix.addObject('FixedConstraint', indices="82 84 85 308 309 496 503 584 681 843 638 640")
+    matrix.addObject('FixedConstraint', indices="465 475 487 590 594 637 638 740 477 675 818 975 1173 508 594 651 657 885 907 948 1149 1383 1446 543 666 920 1041 142 387 542 754 467 470 494 561 562 568 689 710 719 802 837 838 944 965 968 1059 1060 1066 1217 1300 1335 1442 1464")
     
     matrix.addObject('LinearSolverConstraintCorrection')
     
-    matrix.addObject('ConstantForceField',name='FF', indices= "217 218 226 298 469 480 483 516 554 558 565 569 571 607 631 693 721 727 792 834 897 929 930 933 935 1014 1129 1191 1219 1332 1431 1460", force =[0, 0, 0], showArrowSize = "0.00005")
+    matrix.addObject('ConstantForceField',name='FF', indices= "725 806 974 599 627 911 1125 505 519 832 1003 520 541 662 1039 466 581 644 734 892 951 960 1232 1449 1458 1462 224 296 462 541 154 156 393 540 607 978 1014 1105 554 981 1052 1427 758 1096 1256 1430", force =[0, 0, 0], showArrowSize = "0.00003")
+
     
-    matrix.addObject(SpiderController(node=rootNode, pos3 = rootNode.matrix.tetras.position.value))
    
    
-   #matrix.addObject('PositionConstraint', indices= "217 218 226 298 469 480 483 516 554 558 565 569 571 607 631 693 721 727 792 834 897 929 930 933 935 1014 1129 1191 1219 1332 1431 1460",valueType="displacement", value=30, useDirections=[0, 1, 0]) 
+   #matrix.addObject('PositionConstraint', indices= "725 806 974 599 627 911 1125 505 519 832 1003 520 541 662 1039 466 581 644 734 892 951 960 1232 1449 1458 1462 224 296 462 541 154 156 393 540",valueType="displacement", value=50, useDirections=[0, 1, 0]) 
 
 
     
@@ -121,7 +138,7 @@ def createScene(rootNode):
 
     # Add to this empty node a rendering model made of triangles and loaded from a stl file.
     matrixVisu.addObject('MeshSTLLoader', filename=path + "matrix.stl", name="loader")
-    matrixVisu.addObject('OglModel', src="@loader",  color=[0.7, 0.9, 0.3, 0.2]) 
+    matrixVisu.addObject('OglModel', src="@loader",  color=[0.9, 0.9, 0.9, 0.5]) 
 
     # Add a BarycentricMapping to deform the rendering model in a way that follow the ones of the parent mechanical model.
     matrixVisu.addObject('BarycentricMapping')
@@ -137,24 +154,25 @@ def createScene(rootNode):
     # mechanical modelling. In the case of a pneumatic actuation it is a set of positions describing the spider wall.
     filament.addObject('MeshVTKLoader', name='loader', filename=path + 'filament.vtk', rotation=[0, 0, 0])
     filament.addObject('MeshTopology', src='@loader', name='topo')
-    filament.addObject('MechanicalObject', name='filament')
+    filament.addObject('MechanicalObject', name='l_filament')
     filament.addObject('TetrahedronFEMForceField', template='Vec3', name='FEM', method='large', poissonRatio=0.4,
                         youngModulus=9e6)
     filament.addObject('UniformMass', totalMass=0.003)
-
+    #filament.addObject('LinearSolverConstraintCorrection')
 
 
     # This adds a BarycentricMapping. A BarycentricMapping is a key element as it will add a bi-directional link
     #  between the spider wall (surfacic mesh) and the matrix (volumetric mesh) so that movements of the spider's DoFs will be mapped
     #  to the matrix and vice-versa;
     filament.addObject('BarycentricMapping', name='mapping', mapForces=False, mapMasses=False)
+    filament.addObject(SpiderController(node=rootNode, pos_matrix = rootNode.matrix.l_matrix.position.value,  pos_filament = rootNode.matrix.filament.l_filament.position.value))
 
     ##########################################
     # filament Visualization                          #
     ##########################################
     filamentVisu = filament.addChild('visu1')
     filamentVisu.addObject('MeshSTLLoader', filename=path + "filament.stl", name="loader")
-    filamentVisu.addObject('OglModel', src="@loader", color=[2, 3, 2, 9])
+    filamentVisu.addObject('OglModel', src="@loader", color=[0.1, 0.1, 0.1, 0.9])
     filamentVisu.addObject('BarycentricMapping')
 
     return rootNode
