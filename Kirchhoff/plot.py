d1a1331d787aac401f2dfe57708000997fb34d89
Marwa ElDiwiny <marwaeldiwiny1991@gmail.com>
2024-03-29 12:29:11 +0100

    Update plot.py

--- Kirchhoff/plot.py
+++ Kirchhoff/plot.py
@@ -7,32 +7,38 @@ from scipy.signal import savgol_filter
   
 
 
-dataset = pd.read_csv('/home/marwaeldiwiny/Documents/empa02024/onl spirals/w.csv')
+dataset = pd.read_csv('/home/marwaeldiwiny/Documents/sofa plots/one_stage.csv')
 
-time = dataset.iloc[:,0].values
-R1 = dataset.iloc[:,1].values
-R2 = dataset.iloc[:,2].values
-R3 = dataset.iloc[:,3].values
-R4 = dataset.iloc[:,4].values
-R5 = dataset.iloc[:,5].values
-R6 = dataset.iloc[:,6].values
+strain = dataset.iloc[:,0].values
+R14 = dataset.iloc[:,1].values
+R12 = dataset.iloc[:,2].values
+R25 = dataset.iloc[:,3].values
+R36 = dataset.iloc[:,4].values
+R34 = dataset.iloc[:,5].values
+R32 = dataset.iloc[:,6].values
+R16 = dataset.iloc[:,7].values
+R56 = dataset.iloc[:,8].values
+R54 = dataset.iloc[:,9].values
 
 
 
 # Create a new figure and axis
 
-plt.xlabel("time (min)", fontsize="16")
+plt.xlabel("strain (%)", fontsize="16")
 plt.ylabel("Resistance ohm", fontsize="16")
 
 
 # Plot the first set of values on the first y-axis (blue)
 
-plt.plot(time, R1, label = "R1", color='orange', linestyle='solid', marker='*')
-plt.plot(time, R2, label = "R2", color='red', linestyle='solid', marker='*' )
-plt.plot(time, R3, label = "R3", color='green', linestyle='solid', marker='*')
-plt.plot(time, R4, label = "R4", color='fuchsia', linestyle='solid', marker='*')
-plt.plot(time, R5, label = "R5", color='cyan', linestyle='solid', marker='*')
-plt.plot(time, R6, label = "R6", color='blue', linestyle='solid', marker='*')
+plt.plot(strain, R14, label = "R14", color='black', linestyle='solid')
+plt.plot(strain, R12, label = "R12", color='blue', linestyle='solid' )
+plt.plot(strain, R25, label = "R25", color='yellow', linestyle='solid')
+plt.plot(strain, R36, label = "R36", color='black', linestyle='dashed')
+plt.plot(strain, R34, label = "R34", color='red', linestyle='dashed')
+plt.plot(strain, R32 , label = "R32", color='green', linestyle='solid')
+plt.plot(strain, R16, label = "R16", color='red', linestyle='solid')
+plt.plot(strain, R56, label = "R56", color='blue', linestyle='dashed')
+plt.plot(strain, R54, label = "R54", color='green', linestyle='dashed')
 
 
 
